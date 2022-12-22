fa210c1cb8c832eba01c48e74750c6466942f9a8
Ngoc-Phuc Nguyen <77053108+Saizo24@users.noreply.github.com>
2022-12-22 09:43:42 +0100

    added colors

--- MedicineReminder/App.tsx
+++ MedicineReminder/App.tsx
@@ -6,11 +6,25 @@ import CreateEditPage from "./components/pages/CreateEditPage";
 import { de, enGB, registerTranslation } from "react-native-paper-dates";
 import "./i18n/config";
 import { ReminderContextProvider } from "./contexts/ReminderContext";
+import { MD3LightTheme as DefaultTheme, Provider as ThemeProvider } from "react-native-paper";
 
 const Stack = createNativeStackNavigator();
 
 registerTranslation("en-GB", enGB);
 registerTranslation("de", de);
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    primary: {
+      bright: "rgb(132, 202, 132)",
+      main: "rgb(45, 136, 45)",
+      dark: "rgb(15, 100, 15)"
+
+    }
+  }
+}
+
 
 /**
  * To be able to navigate between our different pages, we use this stack navigator, due to its easy usage and integrated header on the page.
@@ -19,18 +33,20 @@ registerTranslation("de", de);
  */
 export default function App() {
   return (
-    <ReminderContextProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Reminder" component={LandingPage} />
-          <Stack.Screen name="New Reminder">
-            {() => <CreateEditPage type="New" />}
-          </Stack.Screen>
-          <Stack.Screen name="Edit Reminder">
-            {() => <CreateEditPage type="Edit" />}
-          </Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ReminderContextProvider>
+    <ThemeProvider>
+      <ReminderContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Reminder" component={LandingPage} />
+            <Stack.Screen name="New Reminder">
+              {() => <CreateEditPage type="New" />}
+            </Stack.Screen>
+            <Stack.Screen name="Edit Reminder">
+              {() => <CreateEditPage type="Edit" />}
+            </Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ReminderContextProvider>
+    </ThemeProvider>
   );
 }
